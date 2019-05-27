03bc30bad853254ea05dcb2cb3e8c9608013c6f2
mrinternauta <feldjesus@gmail.com>
2019-05-26 21:16:19 -0500

    Iniciando

---
+++ Agenda.Logica/LogicaPersonas.cs
@@ -0,0 +1,28 @@
+﻿using Agenda.Datos;
+using Agenda.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Logica
+{
+    public class LogicaPersonas
+    {
+        private RepositorioPersona _repoPersonas;
+        public LogicaPersonas()
+        {
+            _repoPersonas = new RepositorioPersona();
+        }
+        public Persona getPerson (int id)
+        {
+            return _repoPersonas.ObtienePersona(id);
+        }
+        public void InsertPerson ( Persona  person)
+        {
+            _repoPersonas.InsertaPersona(person);
+        }
+
+    }
+}
---
+++ Agenda/Controllers/PersonasController.cs
@@ -0,0 +1,47 @@
+﻿using Agenda.Entidades;
+using Agenda.Logica;
+using Agenda.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private LogicaPersonas _logiPerson;
+        public PersonasController()
+        {
+            _logiPerson = new LogicaPersonas();
+        }
+        public ActionResult Detalle (int id)
+        {
+            var entidad = _logiPerson.getPerson(id);
+            PersonaViewModel personavm = new PersonaViewModel
+            {
+                Id  = entidad.Id,
+                Nombre = entidad.Nombre,
+                Numero = entidad.Numero
+            };
+            return View(personavm);
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear( CreaPersonaViewModel persona)
+        {
+            Persona entidadPersona = new Persona
+            {
+                Nombre = persona.Nombre,
+                Numero = persona.Numero
+            };
+            _logiPerson.InsertPerson(entidadPersona);
+            return RedirectToAction("Detalle", new { id = entidadPersona.Id });
+        }
+
+    }
+}
